2dd69c44e5627c93ab14e34ae05182631c48ce62
Evdokim <60344915+EvdokimNe@users.noreply.github.com>
2026-09-02 19:03:16 +0300

    Readme

--- Assets/SaveSystemExample/SaveExample.cs
+++ Assets/SaveSystemExample/SaveExample.cs
@@ -1,23 +1,26 @@
 using System;
+using System.Runtime.Serialization;
 using SaveSystem.SaveSystem.Core;
 
 // SaveService автоматически использует этот ID и версию в заголовке файла.
 namespace SaveSystemExample
 {
     [SaveType("save-example", version: 1)]
+    [DataContract]
     [Serializable]
     public class SaveExample
     {
-        public string PlayerName = "Player";
-        public int Level = 1;
-        public int Coins;
-        public bool IntroFinished;
+        [DataMember] public string PlayerName = "Player";
+        [DataMember] public int Level = 1;
+        [DataMember] public int Coins;
+        [DataMember] public bool IntroFinished;
     }
 
-// Атрибут необязателен: в этом случае SaveService использует Type.FullName и версию 1.
+// Атрибут SaveType необязателен: в этом случае SaveService использует Type.FullName и версию 1.
     [Serializable]
+    [DataContract]
     public class SaveExampleWithoutAttribute
     {
-        public int HighScore;
+        [DataMember] public int HighScore;
     }
 }
